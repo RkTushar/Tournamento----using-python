8baf88003e7fd23fef1cc2741b9ff5adaca6b666
Rezaul Karim Tushar <53859459+RkTushar@users.noreply.github.com>
2025-06-08 01:23:44 +0600

    Update urls.py

--- tournamento/core/urls.py
+++ tournamento/core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('tournament/<int:tournament_id>/setup/', views.setup_tournament, name='setup_tournament'),
+    # add more as we go
 ]
