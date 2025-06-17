09c707d1808e3377bc1deab433b26f333f8db149
Rezaul Karim Tushar <53859459+RkTushar@users.noreply.github.com>
2025-06-17 11:47:20 +0600

    Update urls.py

--- tournamento/core/urls.py
+++ tournamento/core/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('tournament/<int:tournament_id>/setup/', views.setup_tournament, name='setup_tournament'),
     path('tournament/<int:tournament_id>/', views.tournament_detail, name='tournament_detail'),
     path('match/<int:match_id>/update/', views.update_match_score, name='update_match_score'),
-
 ]
